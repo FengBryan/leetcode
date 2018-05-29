b996540215ec22796c73e3f524218a526e4c7e8f
chunbo <chunbo@teambition.com>
2018-05-29 14:03:26 +0800

    edit longest-substring-without-repeating-characters

--- longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters.js
@@ -3,35 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  const charArr = s.split('')
-  function makeHash (start, arr, subArr = []) {
-    
+  const set = new Set()
+  let pre = 0
+  let last = 0
+  let maxLength = 0
+  while (last < s.length) {
+    if (!set.has(s[last])) {
+      set.add(s[last])
+      last++
+      maxLength = Math.max(maxLength, last - pre)
+    } else {
+      set.delete(s[pre])
+      pre++
+    }
   }
-  const map = charArr.reduce((hash, ele, index) => {
-    hash[ele] = (hash[ele] || 0) + 1
-    makeIndexForHashMap(index, charArr.length - 1, charArr, hash)
-    return hash
-  }, {})
-
-  const result = Object
-    .keys(map)
-    .map(key => {
-      return map[key] === 1 ? key : null
-    })
-    .filter(key => !!key)
-    .map(key => {
-      const charMap = key.split('').reduce((dict, ele, index) => {
-        dict[ele] = (dict[ele] || 0) + 1
-      }, {})
-      let hasRepeatChar = false
-      for (let f in charMap) {
-        hasRepeatChar = charMap[f] > 1
-        if (hasRepeatChar) break
-      }
-
-      if (hasRepeatChar) return null
-    })
-    .filter(ele => !!ele)
-
-  console.log(result)
 }
